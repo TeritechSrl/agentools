c35ba91ef312762742e76517634920af73c5be84
Haimerher <hairo.mh@gmail.com>
2018-01-23 13:10:49 -0400

    ajuste en la estructura y diseno seccion cliente (principal)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,33 +5,59 @@ import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import {MatIconModule} from '@angular/material/icon';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { FormComponent } from './client/form/form.component';
+import { ClientComponent } from './client/client.component';
+import { ClientDetailComponent } from './client/client-detail/client-detail.component';
+
 import {
   MatButtonModule,
-  MatIconModule,
   MatFormFieldModule,
   MatInputModule,
   MatRadioModule,
-  MatSelectModule
+  MatSelectModule,
+  MatTableModule,
+  MatCardModule,
+  MatCheckboxModule
 } from '@angular/material';
 
-import { AppComponent } from './client/app.component';
-import { FormComponent } from './client/form/form.component';
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'client', component: ClientComponent },
+  { path: 'client/:id', component: ClientDetailComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    DashboardComponent,
+    ClientComponent,
+    ClientDetailComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     FormsModule,
     MatFormFieldModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule,
+    MatCardModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/client/client-detail/client-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-detail',
+  templateUrl: './client-detail.component.html'
+})
+export class ClientDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
