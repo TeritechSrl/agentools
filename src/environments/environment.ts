04829ed76248f88859a936ba5144085d423415eb
Haimerher <hairo.mh@gmail.com>
2018-02-16 15:50:24 -0400

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   origin: 'https://apiagentools.azurewebsites.net/api/',
   filescontainer:'https://teritechsrl.blob.core.windows.net/agentools'
+  
 };
