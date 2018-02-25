621e05e9963302e6fe72388f7380ee3f69d973a9
Bernabe Oviedo <berna-b99@hotmail.com>
2018-02-25 19:14:53 -0400

    enviando token en cada request

--- src/app/overriders/interceptorhttp.overrider.ts
+++ src/app/overriders/interceptorhttp.overrider.ts
@@ -1,14 +1,23 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/Rx";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
 import { environment } from "../../environments/environment";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { AuthService } from "../security/service/auth.service";
 @Injectable()
-export class  HttpLogInterceptor implements HttpInterceptor {
+export class HttpLogInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {
+
+  }
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-      request = request.clone({url:environment.origin+request.url});
+    request = request.clone({
+      url: environment.origin + request.url,
+      setHeaders: {
+        Authorization: 'Bearer ' + this.authService.getToken()
+      }
+    });
     return next.handle(request);
   }
 }
